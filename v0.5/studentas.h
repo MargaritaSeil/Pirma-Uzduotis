08acd31b06ddafde0cc30c5651aff16689bcbf08
MargaritaSeil <90559062+MargaritaSeil@users.noreply.github.com>
2021-11-11 21:57:40 +0200

    Update studentas.h

--- v0.5/studentas.h
+++ v0.5/studentas.h
@@ -1,5 +1,5 @@
-#ifndef STUDENTAS_H     //make sure not already exist
-#define STUDENTAS_H     //define
+#ifndef STUDENTAS_H 
+#define STUDENTAS_H
 
 #include <iostream>
 #include <iomanip>
@@ -12,7 +12,6 @@
 #include <numeric>
 #include <list>
 
-//HERE GOES STUFF
 struct studentas{
     std::string vardas, pavarde;
     std::vector <float> paz;
@@ -21,15 +20,11 @@ struct studentas{
     float galutinis_paz;
 };
 
-// this function generates the student list.
-
 void writeTitle(std::ofstream &file);
 void writeResults(studentas, std::ofstream &file);
 void writeGeneratedListTitle(int nPaz, std::ofstream &file);
 void writeGeneratedList(studentas, std::ofstream &file);
-
 void generateList(std::list<studentas> &grupe, int nStudentai, int nPaz);
-// make a function here for the mathies. it can be void and we need a vector to hold the group, like in generateList. you had it right. now you need to name it. you can use grupe here too.
 void pazSkaic(std::list<studentas> &grupe);
 int randomNumber();
 double median(std::vector<float> &vec);
