eb1d1504d1e966e87fee249f52279343dea0099c
MargaritaSeil <90559062+MargaritaSeil@users.noreply.github.com>
2021-11-19 00:46:24 +0200

    Add files via upload

--- v0.5/studentas.h
+++ v0.5/studentas.h
@@ -1,5 +1,5 @@
-#ifndef STUDENTAS_H 
-#define STUDENTAS_H
+#ifndef STUDENTAS_H     //make sure not already exist
+#define STUDENTAS_H     //define
 
 #include <iostream>
 #include <iomanip>
@@ -15,7 +15,6 @@
 struct studentas{
     std::string vardas, pavarde;
     std::vector <float> paz;
-    double med;
     float egz;
     float galutinis_paz;
 };
@@ -24,10 +23,16 @@ void writeTitle(std::ofstream &file);
 void writeResults(studentas, std::ofstream &file);
 void writeGeneratedListTitle(int nPaz, std::ofstream &file);
 void writeGeneratedList(studentas, std::ofstream &file);
+
+void readFile(std::ifstream &file, std::list<studentas> &grupe);
+void readFile(std::ifstream &file, std::vector<studentas> &grupe);
+
 void generateList(std::list<studentas> &grupe, int nStudentai, int nPaz);
+
 void pazSkaic(std::list<studentas> &grupe);
+void pazSkaic(std::vector<studentas> &grupe);
+
 int randomNumber();
-double median(std::vector<float> &vec);
-bool compareStudents(studentas a, studentas b);
+bool compareNames(studentas a, studentas b);
 
 #endif     
