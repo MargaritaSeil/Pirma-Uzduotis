9058e9b234fb48eeb72da205017fbcd2377ae003
MargaritaSeil <90559062+MargaritaSeil@users.noreply.github.com>
2022-01-11 16:17:24 +0200

    Add files via upload

--- v1.0.2/studentas.cpp
+++ v1.0.2/studentas.cpp
@@ -1,27 +1,5 @@
 #include "studentas.h"
 
-void generateList(std::vector<studentas> &grupe, int nStudentai, int nPaz) {
-  studentas temp;
-  float random;
-
-  for (int i=0; i<nStudentai; i++) {
-    temp.vardas = "Vardas" + std::to_string(i+1);
-    temp.pavarde = "Pavarde" + std::to_string(i+1);
-
-    for(int j=0; j<nPaz; j++) {
-      
-      random = (float) randomNumber();
-      temp.paz.push_back(random);
-    }
-    
-    random = (float) randomNumber();
-    temp.egz = random;
-
-    grupe.push_back(temp);
-    temp.paz.clear();
-  }
-}
-
 void pazSkaic(std::list<studentas> &grupe) {
   float vid;
 
@@ -45,17 +23,9 @@ void pazSkaic(std::deque<studentas> &grupe) {
 
   for (auto &kint: grupe) {
     vid=std::accumulate(kint.paz.begin(), kint.paz.end(), 0.0) / kint.paz.size();
+    //kint.med = median(kint.paz);
     kint.galutinis_paz = 0.4 * vid + 0.6 * kint.egz;
   }
-}
-
-int randomNumber() {
-
-  using hrClock = std::chrono::high_resolution_clock;
-  std::mt19937 mt(static_cast<long unsigned int>(hrClock::now().time_since_epoch().count()));
-  std::uniform_int_distribution<int> dist(0, 10);
-  
-  return dist(mt);
 }
 
 bool compareNames(studentas a, studentas b){
@@ -81,34 +51,6 @@ void writeResults(studentas kint, std::ofstream &file){
     file<<std::setw(15)<<kint.vardas<<std::setw(15)<<kint.pavarde;
     file<<std::setw(15)<<std::setprecision(3)<<kint.galutinis_paz<<std::endl;
     }
-  else {
-    std::cout << "negalima įrašyti." ;
-  }
-}
-
-void writeGeneratedListTitle(int nPaz, std::ofstream &file) {
-
-  if(file.is_open()) {
-    file<<std::setw(15)<<std::left<<"Vardas"<<std::setw(15)<<std::left<<"Pavarde";
-    for(int i = 0; i < nPaz; i++) {
-      file<<std::setw(5)<<std::left<<"nd" + std::to_string(i+1);
-    }
-    file<<std::setw(5)<<std::left<<"Egz."<<std::endl;
-  }
-  else {
-    std::cout << "negalima įrašyti." ;
-  }
-}
-
-void writeGeneratedList(studentas kint, std::ofstream &file) {
-  
-  if(file.is_open()) {
-    file<<std::setw(15)<<std::left<<kint.vardas<<std::setw(15)<<std::left<<kint.pavarde;
-    for(int i = 0; i < kint.paz.size(); i++) {
-      file<<std::setw(5)<<std::setprecision(3)<<kint.paz[i];
-    }
-    file<<std::setw(5)<<std::setprecision(3)<<kint.egz<<std::endl;
-  }
   else {
     std::cout << "negalima įrašyti." ;
   }
