e64ee38f079becd0df3459040fefb753a52f4431
MargaritaSeil <90559062+MargaritaSeil@users.noreply.github.com>
2022-01-11 15:57:50 +0200

    Add files via upload

--- v0.5/studentas.h
+++ v0.5/studentas.h
@@ -21,18 +21,13 @@ struct studentas{
 
 void writeTitle(std::ofstream &file);
 void writeResults(studentas, std::ofstream &file);
-void writeGeneratedListTitle(int nPaz, std::ofstream &file);
-void writeGeneratedList(studentas, std::ofstream &file);
 
 void readFile(std::ifstream &file, std::list<studentas> &grupe);
 void readFile(std::ifstream &file, std::vector<studentas> &grupe);
 
-void generateList(std::list<studentas> &grupe, int nStudentai, int nPaz);
-
 void pazSkaic(std::list<studentas> &grupe);
 void pazSkaic(std::vector<studentas> &grupe);
 
-int randomNumber();
 bool compareNames(studentas a, studentas b);
 
 #endif     
